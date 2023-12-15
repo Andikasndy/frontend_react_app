475834a9093788004bed60d763c03c709a3616ad
andika <kentungj97@gmail.com>
2023-12-15 23:53:14 +0700

    sesi 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Intro from './components/intro';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>sesi 1</h1>
+      <Intro name ='Andika Sandi Yudistira'/>
+      <Intro name ='Lisa blackpink'/>
+      
     </div>
   );
 }
---
+++ src/components/intro.js
@@ -0,0 +1,12 @@
+
+function Intro(props) {
+    //console.log('parameter kiriman ;' +props.name)
+  return (
+    <div>
+      <h1>test</h1>
+      <p> halo nama saya : {props.name}</p>
+    </div>
+  );
+}
+
+export default Intro;
