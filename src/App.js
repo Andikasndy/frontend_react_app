fb5d5b333f4e2c6e2da1e228b6c78e6331b0bf01
andika <kentungj97@gmail.com>
2023-12-20 23:22:41 +0700

    hooks pada react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import Intro from './components/intro';
 import Button from './components/button';
 import Footer from './components/footer';
-import React, { useState } from 'react';
 import './App.css'
 // router
 import Home from './page/home';
 import About from './page/about';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Notfund from './page/notfund';
-
+// Hooks
+import React, { useState,useEffect } from 'react';
 const App = () => {
   const nama="andika sandi yudistira";
 
   const clicked = () =>{
     return alert("button di pencet");
   };
-  // uabah state
+  // state pada react js
   const [getSeeUoll, setSeeUoll] = useState("");
   const cangeSeeUoll =()=>{
     setSeeUoll("SEE U AGAIN");
   };
-
+ 
+  //props
   const paragrap = () =>{
     return (
     <div>
@@ -28,6 +29,20 @@ const App = () => {
     </div>
     );
   }
+  
+  
+  //Hooks state 2
+  const [pacarSaya, setPacar]= useState(1)
+  const [getMetiont, setMention]=useState("")
+  useEffect(() =>{
+    if(pacarSaya > 1){
+    setMention('nah ketahuan loo dasar playboy')
+    } else if  (pacarSaya === 0){
+    setMention('yahahaha jomblo yaa')
+    } else{
+    setMention('nah gitu dong setia')
+    }
+  },[pacarSaya])
 
   return (
     //state & props component
@@ -39,15 +54,23 @@ const App = () => {
     // </div>
 
     //router dom
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='About' element={<About/>}/>
-          <Route path='*' element={<Notfund/>}/>
-        </Routes>
-      </Router>
-    </div>
+    // <div>
+    //   <Router>
+    //     <Routes>
+    //       <Route path='/' element={<Home/>}/>
+    //       <Route path='About' element={<About/>}/>
+    //       <Route path='*' element={<Notfund/>}/>
+    //     </Routes>
+    //   </Router>
+    // </div>
+    
+    // hooks state 
+    <>
+    <h2>{getMetiont}</h2>
+    <h1>saya memiliki pacar :{pacarSaya}</h1>
+    <button onClick={()=>setPacar((prev) => prev + 1)}>Tambah pacar</button>
+    <button onClick={()=>setPacar((prev) => prev - 1)}>Kurangi pacar</button>
+    </>
   );
 }
 
